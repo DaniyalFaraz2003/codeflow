15af7f649d59318eb5bd77320111fb39223c8168
DaniyalFaraz2003 <daniyalfaraz2003@gmail.com>
2025-05-06 00:06:44 +0500

    update backend apis calls

--- codeflow-frontend/src/services/api.js
+++ codeflow-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://codeflow-backend-mu.vercel.app/api/";
+const API_URL = "https://codeflow-backend-henna.vercel.app/api/";
 
 const api = axios.create({
     baseURL: API_URL,
